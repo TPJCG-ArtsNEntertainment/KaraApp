b120adcc34c4b7bbafd3bf7070f4901bc013da43
TPJCG-ArtsNEntertainment <ae.tpjcg@gmail.com>
2024-01-29 10:13:14 +0800

    remember androidID

--- app/src/main/java/mdad/networkdata/karaapp/Login.java
+++ app/src/main/java/mdad/networkdata/karaapp/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,15 +22,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity {
-    private String uid, is_staff, username;
+    private String uid, is_staff, username, androidId;
     private EditText inputEmail, inputPassword;
     private Button btnLogin, btnRegister;
+    private static String url_verify_device = MainMenu.ipBaseAddress + "verify_deviceVolley.php";
     private static String url_verify_user = MainMenu.ipBaseAddress+"verify_userVolley.php";
+    private static String url_update_device = MainMenu.ipBaseAddress+"update_deviceVolley.php";
+    private final int verify_device = 1, verify_user =2, update_device=3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        Map<String, String> param_verify = new HashMap<>();
+        param_verify.put("token", androidId);
+        postData(url_verify_device, param_verify, verify_device);
 
         inputEmail = (EditText) findViewById(R.id.inputEmail);
         inputPassword = (EditText) findViewById(R.id.inputPassword);
@@ -40,10 +50,12 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 String email = inputEmail.getText().toString();
                 String password = inputPassword.getText().toString();
+
                 Map<String, String> param_login = new HashMap<>();
                 param_login.put("email", email);
                 param_login.put("password", password);
-                postData(url_verify_user, param_login);
+                param_login.put("token", androidId);
+                postData(url_verify_user, param_login, verify_user);
             }
         });
         btnRegister = (Button) findViewById(R.id.btnRegister);
@@ -55,42 +67,89 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-    public void postData(String url, Map params) {
+    public void postData(String url, Map params, final int requestType) {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        if (response.equals("Error password")) {
-                            Toast.makeText(getApplicationContext(), "Wrong password",
+                        System.out.println(response);
+                        if (requestType == verify_device){
+                            if (response.equals("Error, false request.")) {
+                                Toast.makeText(getApplicationContext(), "Error in verify device",
+                                        Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            if (response.equals("Error: Device not registered.")) {
+                                return; //Account not logged in before.
+                            }
+                            String[] user = response.split("\\|");
+                            String[] details = user[0].split(";");
+                            String login_uid = details[0];
+                            String login_email = details[1];
+                            String login_password = details[2];
+                            String login_name = details[3];
+                            String login_is_staff = details[4];
+                            String login_token = details[5];
+
+                            uid = login_uid;
+                            is_staff = login_is_staff;
+
+                            finish();
+                            Intent intent = new Intent(Login.this, MainMenu.class);
+                            intent.putExtra("uid", uid);
+                            intent.putExtra("is_staff", is_staff);
+                            if (login_name == null) username = login_email;
+                            else username = login_name;
+                            intent.putExtra("username", username);
+                            Toast.makeText(getApplicationContext(), "Logging in to "+username,
                                     Toast.LENGTH_LONG).show();
-                            return;
-                        }if (response.equals("Error email")) {
-                            Toast.makeText(getApplicationContext(), "Wrong email",
-                                    Toast.LENGTH_LONG).show();
-                            return;
+                            startActivity(intent);
                         }
-                        String[] user = response.split("\\|");
-                        String[] details = user[0].split(";");
-                        String login_uid = details[0];
-                        String login_email = details[1];
-                        String login_password = details[2];
-                        String login_name = details[3];
-                        String login_is_staff = details[4];
+                        if (requestType == verify_user){
+                            if (response.equals("Error password")) {
+                                Toast.makeText(getApplicationContext(), "Wrong password",
+                                        Toast.LENGTH_LONG).show();
+                                return;
+                            }if (response.equals("Error email")) {
+                                Toast.makeText(getApplicationContext(), "Wrong email",
+                                        Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            String[] user = response.split("\\|");
+                            String[] details = user[0].split(";");
+                            String login_uid = details[0];
+                            String login_email = details[1];
+                            String login_password = details[2];
+                            String login_name = details[3];
+                            String login_is_staff = details[4];
+//                            String login_token = details[5];
 
-                        uid = login_uid;
-                        is_staff = login_is_staff;
+                            uid = login_uid;
+                            is_staff = login_is_staff;
 
-                        finish();
-                        Intent intent = new Intent(Login.this, MainMenu.class);
-                        intent.putExtra("uid", uid);
-                        intent.putExtra("is_staff", is_staff);
-                        if (login_name == null) username = login_email;
-                        else username = login_name;
-                        intent.putExtra("username", username);
-                        Toast.makeText(getApplicationContext(), "Logging in to "+username,
-                                Toast.LENGTH_LONG).show();
-                        startActivity(intent);
+                            Map<String, String> param_update = new HashMap<>();
+                            param_update.put("uid",login_uid);
+                            param_update.put("token", androidId);
+                            postData(url_update_device, param_update, update_device);
+
+                            finish();
+                            Intent intent = new Intent(Login.this, MainMenu.class);
+                            intent.putExtra("uid", uid);
+                            intent.putExtra("is_staff", is_staff);
+                            if (login_name == null) username = login_email;
+                            else username = login_name;
+                            intent.putExtra("username", username);
+                            Toast.makeText(getApplicationContext(), "Logging in to "+username,
+                                    Toast.LENGTH_LONG).show();
+                            startActivity(intent);
+                        }
+                        if (requestType == update_device){
+                            if (response.equals("Error")) {
+                                Toast.makeText(getApplicationContext(), "Error in updating database",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
--- app/src/main/java/mdad/networkdata/karaapp/MainMenu.java
+++ app/src/main/java/mdad/networkdata/karaapp/MainMenu.java
@@ -1,5 +1,6 @@
 package mdad.networkdata.karaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -10,9 +11,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainMenu extends AppCompatActivity {
     public static String uid, is_staff, username;
@@ -22,6 +33,7 @@ public class MainMenu extends AppCompatActivity {
     private FragmentStateAdapter pagerAdapter;
     private String[] titles = new String[]{"Session", "History", "Player", "Lyrics"};
     private static final int NUM_PAGES = 4;
+    private static String url_update_device = MainMenu.ipBaseAddress+"update_deviceVolley.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,12 +106,18 @@ public class MainMenu extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         // Array of menu items with their corresponding destination classes
-        int[] menuItems = {R.id.item1, R.id.item2, R.id.item3, R.id.item4, R.id.item5};
-        Class<?>[] destinationClasses = {MainMenu.class, UserManagement.class, ProfileSettings.class, RulesAndRegulations.class, Login.class};
+        int[] menuItems = {R.id.item1, R.id.item2, R.id.item3, R.id.item4};
+        Class<?>[] destinationClasses = {MainMenu.class, UserManagement.class, ProfileSettings.class, RulesAndRegulations.class};
         // Iterate over menu items and check conditions
         for (int i = 0; i < menuItems.length; i++) {
-            if (id == menuItems[i]) {
+            if (id == R.id.item5){
+                Map<String, String> param_update = new HashMap<>();
+                param_update.put("uid", uid);
+                param_update.put("token", "");
+                postData(url_update_device, param_update);
+            } else if (id == menuItems[i]) {
                 // Start the activity for the selected menu item
+                finish();
                 startActivityIntent(destinationClasses[i]);
                 return true;
             } else if (id == android.R.id.home) {
@@ -116,5 +134,30 @@ public class MainMenu extends AppCompatActivity {
         intent.putExtra("is_staff", is_staff);
         intent.putExtra("username", username);
         startActivity(intent);
+    }
+    public void postData(String url, Map params) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Error, false request.")) {
+                    Toast.makeText(getApplicationContext(), "Error in verify device",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                finish();
+                startActivityIntent(Login.class);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"Error in accessing database",Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() { return params; }
+        };
+        requestQueue.add(stringRequest);
     }
 }
